dc6d4b24a54b62f9314faea030bcd5b37489aee4
Sarisia <sarisiaice@gmail.com>
2019-11-23 12:55:51 +0900

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Actions Status Discord
+
+## About
+
+This is a Github Actions action to notify CI status to Discord.
+
+## Usage
+
+```yaml
+- name: Post status to Discord
+  uses: sarisia/actions-status-discord@v1
+  # make sure to set this `always()` 
+  # or status failure and cancelled won't be notified!
+  if: always()
+  with:
+    # provide discord webhook via either inputs or env.DISCORD_WEBHOOK
+    webhook: ${{ secrets.DISCORD_WEBHOOK }}
+    status: ${{ job.status }}
+    job: deploy to github pages
+    description: build pages and deploy to github pages!
+```
+
+![notification](https://user-images.githubusercontent.com/33576079/69472655-332b2880-0df0-11ea-9c33-6add9fca62e9.png)
+
+## Environment Variables
+
+| Key | Value | Description |
+| - | - | - |
+| DISCORD_WEBHOOK | Discord webhook endpoind like:<br>`https://discordapp.com/api/webhooks/...` | You can provide webhook via inputs either.
+
+## Inputs
+
+| Key | Default | Required | Description |
+| - | - | - | - |
+| webhook | | no | Discord webhook endpoind like:<br>`https://discordapp.com/api/webhooks/...`<br>You can provide webhook via env either. If both is set, inputs will be preferred. |
+| status | `success` | no | Accepts `success`, `failure` and `cancelled`. Set to `${{ job.status }}` is recommended. |
+| description | | no | Description included in message. See example above. |
+| job | | no | Job name included in message title. See example above. |
+| nofail | `true` | no | **MUST BE STRING!!!**<br>Accepts `true` and `false`. This action won't make workflow failed by default. If set to `false`, this action will set status failed when failed to notify. |
---
+++ src/main.ts
@@ -0,0 +1,97 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import axios from 'axios'
+
+interface StatusOption {
+    status: string
+    color: number
+}
+
+const statusOpts: Record<string, StatusOption> = {
+    success: {
+        status: 'Success',
+        color: 0x28A745
+    },
+    failure: {
+        status: 'Failure',
+        color: 0xCB2431
+    },
+    cancelled: {
+        status: 'Cancelled',
+        color: 0xDBAB09
+    }
+}
+
+async function run() {
+    const nofail: boolean = core.getInput('nofail').trim().toLowerCase() == 'true'
+    const webhook: string = core.getInput('webhook') || process.env.DISCORD_WEBHOOK || ''
+    if (!webhook) {
+        logError('No webhook endpoint is given', nofail)
+        process.exit()
+    }
+    const status: string = core.getInput('status').toLowerCase()
+    if (!(status in statusOpts)) {
+        logError('Invalid status value', nofail)
+        process.exit()
+    }
+    const description: string = core.getInput('description')
+    const job: string = core.getInput('job')
+
+    try {
+        await axios.post(webhook, getPayload(status, description, job))
+    } catch (e) {
+        logError(e, nofail)
+    }
+}
+
+function logError(msg: string, nofail: boolean): void {
+    nofail ? core.error(msg) : core.setFailed(msg)
+}
+
+function getPayload(status: string, description: string, job: string): object {
+    const ctx = github.context
+    const { owner, repo } = ctx.repo
+    const { eventName, sha, ref, workflow, actor } = ctx
+    const repoURL = `https://github.com/${owner}/${repo}`
+    const workflowURL = `${repoURL}/commit/${sha}/checks`
+
+    let payload = {
+        embeds: [{
+            title: statusOpts[status].status + (job ? `: ${job}` : ''),
+            color: statusOpts[status].color,
+            description: description || null,
+            fields: [
+                {
+                    name: 'Repository',
+                    value: `[${owner}/${repo}](${repoURL})`,
+                    inline: true
+                },
+                {
+                    name: 'Ref',
+                    value: ref,
+                    inline: true
+                },
+                {
+                    name: 'Event',
+                    value: eventName,
+                    inline: true
+                },
+                {
+                    name: 'Triggered by',
+                    value: actor,
+                    inline: true
+                },
+                {
+                    name: 'Workflow',
+                    value: `[${workflow}](${workflowURL})`,
+                    inline: true
+                }
+            ]
+        }]
+    }
+
+    core.debug(`payload: ${JSON.stringify(payload)}`)
+    return payload
+}
+
+run()
