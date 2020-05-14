287eaa3046a4e327eed1f9eb9c740d4834e57cf0
Sarisia <sarisiaice@gmail.com>
2020-05-13 17:30:09 -0700

    refactor: refactoring (#3)
    * workflow: test release branch
    
    * chore: delete unused workflows
    
    * chore(workflow): add run
    
    * refactor: overall
    
    * refactor: fix status scope
    
    * refactor: fix webhook
    
    * refactor: fix webhook empty string
    
    * fix: formatter
    
    * wip
    
    * done

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+import * as core from '@actions/core'
+
+const NOFAIL: boolean = core.getInput('nofail').trim().toLowerCase() === 'true'
+
+export function logError(msg: string) {
+    NOFAIL ? core.error(msg) : core.setFailed(msg)
+}
+
+export function logInfo(msg: string) {
+    core.debug(msg)
+}
