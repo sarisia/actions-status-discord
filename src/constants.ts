18ebe1b0789201bb9163dc3ab0b212693e750a94
Michael H <git@riskymh.dev>
2024-03-04 03:39:02 +1100

    embed description is actually 4096 characters (#520)

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // Discord Embed limits
 // https://discord.com/developers/docs/resources/channel#embed-object-embed-limits
 export const MAX_EMBED_TITLE_LENGTH = 256
-export const MAX_EMBED_DESCRIPTION_LENGTH = 2048
+export const MAX_EMBED_DESCRIPTION_LENGTH = 4096
 export const MAX_EMBED_FIELD_NAME_LENGTH = 256
 export const MAX_EMBED_FIELD_VALUE_LENGTH = 1024
 
